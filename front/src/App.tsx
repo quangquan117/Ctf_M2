0e27c152a9645b348f89a0b3cdf4a8599a239518
Aymane Sarhou <sarhouaymane2004@gmail.com>
2026-05-06 11:41:05 +0200

    feat(front): configuration du routing principal de l'application

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
-  return <div>Application en cours de configuration...</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/connexion" element={<Login />} />
+          <Route path="/inscription" element={<Register />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
